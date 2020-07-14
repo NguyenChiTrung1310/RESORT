e4d4c5849dcf1ea47ff3ee3e5134940251181cc8
Nguyễn Chí Trung <nguyenchitrung1310@gmail.com>
2020-07-14 22:00:25 +0700

    In progress: Hero part

---
+++ resort/src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import "./App.css";
+import Home from './pages/Home';
+import Rooms from './pages/Room';
+import SingleRoom from './pages/SingleRoom';
+import Error from './pages/Error';
+
+
+import {
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import Navbar from './components/Navbar';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/rooms/" component={Rooms} />
+        <Route exact path="/rooms/:slug" component={SingleRoom} />
+        <Route component={Error} />
+      </Switch>
+
+    </div>
+  );
+}
+
+export default App;
